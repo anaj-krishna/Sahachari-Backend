6227afe4361e38920b5e5677d82d12cbc9a6a78b
Vinayak S <vinayaksukhalal@gmail.com>
2026-04-29 18:30:26 +0530

    upi added

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: '*',
-    credentials: true,
+  origin: '*',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
   });
   setupApp(app);
   await app.listen(process.env.PORT || 3000, '0.0.0.0');
